93522e8f691359085cd6c2a3a64f375a56426306
HeXudong <xudonghe@zhulux.com>
2016-05-25 16:19:34 +0800

    add application.js

---
+++ front_web/src/entries/application.js
@@ -0,0 +1,6 @@
+import './index.html';
+import './index.less';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { browserHistory } from 'react-router';
+import App from '../components/App';
